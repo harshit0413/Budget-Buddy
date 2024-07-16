9a8e58363c3b962c47b088c7ca2a1202ac5a195b
harshit0413 <harshitjain413@gmail.com>
2024-07-17 01:16:47 +0530

    .

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 
 export default {
-  schema: "./utlis/schema.jsx",
+  schema: "./utils/schema.jsx",
   dialect: 'postgresql',
   dbCredentials: {
     url: 'postgresql://neondb_owner:rgzfdK97sFmb@ep-lucky-heart-a59d5p3l.us-east-2.aws.neon.tech/Budget-Buddy?sslmode=require'
